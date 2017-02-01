a1d0340d6bf64d8f256171d0a293b10d90fd603d
Julia Chen Jing <juliachenjing@gmail.com>
2017-01-31 21:35:19 -0600

    Update LongestCommonSubsequence.java

--- LongestCommonSubsequence.java
+++ LongestCommonSubsequence.java
@@ -1,9 +1,19 @@
 public class LCS {
 
 
+	 private static int[] lookUp;
+	 public static  int countRecursive=0;
+	 public static  int countMemoized=0;
+	 
 	public static void main(String [] args)
 	{
-		System.out.println(longestCommonSubsequence("abcd","abcde"));
+	
+		String A="abcde";
+		String B="abcd";
+		 lookUp = new int[Math.max(A.length(),B.length())];        // create and initialize array
+	       for (int i=0; i<Math.max(A.length(),B.length()); i++)        
+	    	      lookUp[i] =Integer.MAX_VALUE;
+		System.out.println(longestCommonSubsequence(A,B));
 	}
 		
 	
@@ -17,11 +27,21 @@ public class LCS {
         int f[][] = new int[n + 1][m + 1];
         for(int i = 1; i <= n; i++){
             for(int j = 1; j <= m; j++){
-                f[i][j] = Math.max(f[i - 1][j], f[i][j - 1]);
+               /* f[i][j] = Math.max(f[i - 1][j], f[i][j - 1]);
                 if(A.charAt(i - 1) == B.charAt(j - 1))
-                    f[i][j] = f[i - 1][j - 1] + 1;
+                    f[i][j] = f[i - 1][j - 1] + 1;*/
+            	helper(A,B,i,j,f);
             }
         }
         return f[n][m];
     }
+    
+    public static int [][] helper(String A,String B, int i,int j, int [] [] f)
+    {
+    	  f[i][j] = Math.max(f[i - 1][j], f[i][j - 1]);
+          if(A.charAt(i - 1) == B.charAt(j - 1))
+              f[i][j] = f[i - 1][j - 1] + 1;
+    	return f;
+    }
 }
+
