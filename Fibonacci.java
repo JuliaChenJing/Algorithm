a33fa348cc164b7117b7faa2e1078ead67329e51
Julia Chen Jing <juliachenjing@gmail.com>
2017-01-31 18:58:08 -0600

    Update Fibonacci.java

--- Fibonacci.java
+++ Fibonacci.java
@@ -1,22 +1,34 @@
 //import java.util.Scanner;  
   
 public class Fibonacci {  
+	
+	 public static  int countRecursive=0;
   
     public static void main(String[] args) {  
       //  Scanner scanner = new Scanner(System.in);  
       //  System.out.println("Please input this fibonacci n:");  
-      //  int n = scanner.nextInt(); // 假设输入为大于零的整数  
+      //  int n = scanner.nextInt(); // input an integer which is bigger than 0
        int  n=30;
-          
-        for(int i = 1; i <= n; i++){  
-            System.out.print(fibonacci(i)+" ");  
-        }  
+      
+      //  for(int i = 1; i <= n; i++){  
+      //      System.out.print(fibonacci(i)+" ");  
+      //  }  
+        
+        System.out.println(" fibonacci(30)=" +fibonacci(n) );
+        System.out.println("countRecursive="+countRecursive);
+        
+        
        
     }  
       
-    // 递归实现方式  
+    // recursive way
     public static int fibonacci(int n){  
-        if(n <= 2){  
+    	countRecursive++;
+        if(n == 0){  
+        	
+            return 0;  
+        }
+         else if(n == 1){  
         	
             return 1;  
         }else{  
