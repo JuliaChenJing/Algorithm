a6f37510b495b687a925696eb6f3fea7c9360e62
Julia Chen Jing <juliachenjing@gmail.com>
2017-02-21 23:42:22 -0600

    Create reverseCString.java

---
+++ reverseCString.java
@@ -0,0 +1,30 @@
+import java.util.HashMap;
+
+public class ReverseCString {
+
+
+    public static String reverseCString(String s) 
+    {
+    	if(s==null||s.length()==0)
+    	return null;
+    	StringBuffer re=new StringBuffer ();
+    	
+    	for(int i=s.length()-1;i>=0;i--)
+    	re.append(s.charAt(i));
+  
+    	return re.toString();
+    		
+  }
+    
+    public static void main(String []args)
+    {
+    	System.out.println(reverseCString("apple "));
+    	
+   
+    }
+	
+}
+
+ /*
+ elppa
+  */
