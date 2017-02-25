daa726f65b4abf8235fdc03ece3b3cc631b0886d
Julia Chen Jing <juliachenjing@gmail.com>
2017-02-24 22:02:59 -0600

    Update findNth.java

--- findNth.java
+++ findNth.java
@@ -1,38 +1,61 @@
+import java.util.HashSet;
+import java.util.Hashtable;
+import java.util.Set;
+
 public class FindNth {
 
-	// If we can use a buffer, we can keep track of elements in a hashtable and
-	// remove any dups
+	//我自己想的
 	public static Node findNth(Node head, int N) {
-		
-		if(head==null)
+
+		if (head == null)
 			return null;
-		int length=0;
-		Node n=head;
-		Node re=null;
-		while(n!=null)
-		{
-			n=n.next;
+		int length = 0;
+		Node n = head;
+		Node re = null;
+		while (n != null) {
+			n = n.next;
 			length++;
-			
+
 		}
-		
-		System.out.println("length="+length);
-		
-		if(length<N)
+
+		System.out.println("length=" + length);
+
+		if (length < N)
 			return null;
-		int j=1;
-		Node m=head;
-		while(j<length-N)
-		{
-			re=m.next;
+		int j = 1;
+		Node m = head;
+		while (j < length - N) {
+			re = m.next;
 			j++;
-			m=m.next;
+			m = m.next;
 		}
-			
-		
+
 		return re;
 	}
 
+	//书上的答案，不好理解啊
+	public static Node nthToLast(Node head, int n) {
+		if (head == null || n < 1)
+		{
+			return null;
+		}
+		
+		Node p1 = head;
+		Node p2 = head;
+		for (int j = 0; j < n - 1; ++j) { // skip n‐1 steps ahead
+			if (p2 == null) {
+				return null; // not found since list size < n
+			}
+			
+			p2 = p2.next;
+		}
+		
+		while (p2.next != null) {
+			p1 = p1.next;
+			p2 = p2.next;
+		}
+		return p1;
+	}
 
 	public static void main(String[] args) {
 		Node a = new Node(1);
@@ -53,9 +76,13 @@ public class FindNth {
 			n = n.next;
 		}
 
-		Node re=findNth(a,2);
+		Node re = findNth(a, 2);
 
 		System.out.println(re.data);
+		
+		Node re_2 = nthToLast(a, 2);
+
+		System.out.println(re_2.data);
 	}
 }
 
@@ -93,7 +120,7 @@ class Node {
 }
 
 /*
-BEFORE: 
+ * BEFORE: 
 1--->
 2--->
 3--->
@@ -102,4 +129,6 @@ BEFORE:
 6--->
 length=6
 4
+5
+
  */
