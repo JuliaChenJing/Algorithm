8e6f8bfdbda5beba2d1f91e8ca3dccbe8d017dbc
Julia Chen Jing <juliachenjing@gmail.com>
2017-02-28 22:20:06 -0600

    Create IsBalanced.java

---
+++ IsBalanced.java
@@ -0,0 +1,45 @@
+
+public class IsBalanced {
+
+	public static int maxDepth(TreeNode root) {
+		if (root == null) {
+			return 0;
+		}
+		return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
+	}
+
+	public static int minDepth(TreeNode root) {
+		if (root == null) {
+			return 0;
+		}
+		return 1 + Math.min(minDepth(root.left), minDepth(root.right));
+	}
+
+	public static boolean isBalanced(TreeNode root) {
+		return (maxDepth(root) - minDepth(root) <= 1);
+	}
+	
+	public static void main(String [] args)
+	{
+		TreeNode root=new TreeNode(1);
+		root.left=new TreeNode(2);
+		root.right=new TreeNode(3);
+		root.right.right=new TreeNode(5);
+		root.right.right.right=new TreeNode(7);
+		System.out.println(isBalanced(root));
+	}
+}
+
+class TreeNode {
+	TreeNode left;
+	TreeNode right;
+	
+	TreeNode(int v)
+	{
+		value=v;
+	}
+	int value;
+}
+/*
+false
+*/
