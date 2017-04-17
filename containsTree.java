e3bec444777be166bca68108572c471113051539
Julia Chen Jing <juliachenjing@gmail.com>
2017-04-16 22:39:48 -0500

    Create containsTree.java

---
+++ containsTree.java
@@ -0,0 +1,44 @@
+
+public class BinaryTree {
+
+	boolean containsTree(TreeNode t1, TreeNode t2) {
+		if (t2 == null)
+			return true; // The empty tree is always a subtree
+		else
+			return subTree(t1, t2);
+	}
+
+	//the helper class with recursion: check if r2 is a subtree of r1
+	boolean subTree(TreeNode r1, TreeNode r2) {
+		if (r1 == null)
+			return false; // big tree empty & subtree still not found.
+		if (r1.data == r2.data) {
+			if (matchTree(r1, r2))
+				return true;
+		}
+		return (subTree(r1.left, r2) || subTree(r1.right, r2));
+	}
+
+	//if two trees are all the same
+	boolean matchTree(TreeNode r1, TreeNode r2) {
+		if (r2 == null && r1 == null)
+			return true; // nothing left in the subtree
+		if (r1 == null || r2 == null)
+			return false; // big tree empty & subtree still not found
+		if (r1.data != r2.data)
+			return false; // data doesn’t match
+		return (matchTree(r1.left, r2.left) && matchTree(r1.right, r2.right));
+	}
+}
+
+class TreeNode {
+	TreeNode left;
+	TreeNode right;
+	TreeNode parent;
+
+	TreeNode(int v) {
+		data = v;
+	}
+
+	int data;
+}
